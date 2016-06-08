0f1831afef2deb123606919b5a23f0c0db42666b
Pablo <hello@pabloheimplatz.de>
2016-06-08 16:25:55 +0200

    test 1s clock

--- alarm clock/clkgen.vhd
+++ alarm clock/clkgen.vhd
@@ -21,7 +21,7 @@ architecture behave of clkgen is
 
 signal count1ms : integer range 0 to 999 := 0;
 signal count500ms : integer range 0 to 499 := 0;
-signal count1s : integer range 0 to 100 := 0;
+signal count1s : integer range 0 to 999 := 0;
 
 signal tmpclk1ms : std_logic; -- tmp clk1ms OUT mapping
 signal tmpclk500ms : std_logic; -- tmp clk500ms OUT mapping
@@ -31,6 +31,7 @@ begin
 
 clk1ms <= tmpclk1ms;
 clk500ms <= tmpclk500ms;
+clk1s <= tmpclk1s;
 
 -- http://vhdlguru.blogspot.de/2010/03/digital-clock-in-vhdl.html 
 -- generate a 1ms clock
@@ -55,30 +56,37 @@ begin
   	if reset='0' then 
       count500ms <= 0;
       tmpclk500ms <= '0';
+      count1s <= 0;
+      tmpclk1s <= '0';
 	elsif tmpclk1ms='1' and tmpclk1ms'event then
 		if(count500ms = 249) then
 			tmpclk500ms <= not tmpclk500ms;
 			count500ms <= 0;
 		else
 			count500ms <= count500ms+1;
+			count1s <= count1s+1;
+		end if;
+		if(count1s = 499) then
+			tmpclk1s <= not tmpclk1s;
+			count1s <= 0;
 		end if;
 	end if;
 end process;
 
 -- generate a 1s clock
-gen1s: process(reset, tmpclk1s)
-begin
-  	if reset='0' then 
-      count1s <= 0;
-      tmpclk1s <= '0';
-	elsif tmpclk500ms='1' and tmpclk500ms'event then
-		if(count1s = 50) then
-			tmpclk1s <= not tmpclk1s;
-			count1s <= 0;
-		else
-			count1s <= count1s+50;
-		end if;
-	end if;
-end process;
+--gen1s: process(reset, tmpclk1s)
+--begin
+--  	if reset='0' then 
+--      count1s <= 0;
+--      tmpclk1s <= '0';
+--	elsif tmpclk500ms='1' and tmpclk500ms'event then
+--		if(count1s = 50) then
+--			tmpclk1s <= not tmpclk1s;
+--			count1s <= 0;
+--		else
+--			count1s <= count1s+50;
+--		end if;
+--	end if;
+-- end process;
 
 end architecture behave;
