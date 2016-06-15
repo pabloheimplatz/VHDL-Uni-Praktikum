2b82c4132f81bd7a18d8e3deac711c71dfff0639
Pablo <hello@pabloheimplatz.de>
2016-06-15 16:59:20 +0200

    fix clock

--- alarm clock/clock.vhd
+++ alarm clock/clock.vhd
@@ -5,6 +5,8 @@
 ------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 
 entity clock is
   port(	reset		: in  std_logic;	-- async. reset		L
